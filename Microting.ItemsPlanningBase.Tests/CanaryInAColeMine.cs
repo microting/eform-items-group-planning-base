39c5ee2905cb8e37d91f666201c8b4954404e249
René Schultz Madsen <rm@microting.dk>
2019-05-01 09:10:53 +0200

    Adding the first test.

---
+++ Microting.ItemsPlanningBase.Tests/CanaryInAColeMine.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Microting.ItemsPlanningBase.Tests
+{
+    [TestFixture]
+    public class CanaryInAColeMine
+    {
+        [Test]
+        public void CanPeep()
+        {
+            Assert.True(true);
+        }
+    }
+}
