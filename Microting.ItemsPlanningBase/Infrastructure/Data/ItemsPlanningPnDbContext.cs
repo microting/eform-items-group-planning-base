bb78b8f5426046fe7d08102684b8c3dc4fd23f9e
René Schultz Madsen <rm@microting.dk>
2019-10-29 08:39:13 +0100

    Adding the new PluginGroupPermissionVersions

--- Microting.ItemsPlanningBase/Infrastructure/Data/ItemsPlanningPnDbContext.cs
+++ Microting.ItemsPlanningBase/Infrastructure/Data/ItemsPlanningPnDbContext.cs
@@ -52,5 +52,6 @@ namespace Microting.ItemsPlanningBase.Infrastructure.Data
         public DbSet<PluginConfigurationValueVersion> PluginConfigurationValueVersions { get; set; }
         public DbSet<PluginPermission> PluginPermissions { get; set; }
         public DbSet<PluginGroupPermission> PluginGroupPermissions { get; set; }
+        public DbSet<PluginGroupPermissionVersion> PluginGroupPermissionVersions { get; set; }
     }
 }
