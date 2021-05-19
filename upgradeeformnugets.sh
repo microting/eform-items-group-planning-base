0d09501e9ff0849bdc69c350ab479fa0e3ab06cc
René Schultz Madsen <rm@microting.com>
2021-05-19 19:07:33 +0000

    Adding the EF packages to the upgrade script.

--- upgradeeformnugets.sh
+++ upgradeeformnugets.sh
@@ -6,7 +6,7 @@ if (( "$GIT_STATUS" > 0 )); then
 	cd Microting.ItemsGroupPlanningBase
   CURRENT_NUMBER_OF_COMMITS=`git log --oneline | wc -l`
 
-	PACKAGES=('Microting.eForm' 'Microting.eFormApi.BasePn')
+	PACKAGES=('Microting.eForm' 'Microting.eFormApi.BasePn' 'Microsoft.EntityFrameworkCore.Design')
 	PROJECT_NAME='Microting.ItemsGroupPlanningBase.csproj'
 	REPOSITORY='eform-items-group-planning-base'
 
